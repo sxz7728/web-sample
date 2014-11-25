107d9429cdb0e460e7d849cc00e796cabf033b1b
sxz7728 <sxz7728@sohu.com>
2014-11-25 18:59:44 +0800

    add

---
+++ web-sample-resources/src/main/webapp/js/utilities.js
@@ -0,0 +1,28 @@
+$.utilities = {
+	fullUrl : function(url) {
+		return url.charAt(0) == '/' ? globals.APP_NAME + url : url;
+	},
+
+	ajax : function(s) {
+		s = jQuery.extend({
+			params : {}
+		}, s);
+
+		$.ajax({
+			type : "post",
+			url : fullUrl(s.url),
+			data : s.params,
+			success : function(result) {
+				if (result.success) {
+					if (s.success) {
+						s.success(result);
+					}
+				} else {
+					if (result.error) {
+						alert(result.error);
+					}
+				}
+			}
+		});
+	}
+};

0e8efd5e6fa700469e3fdb79bf4a97e1f0034da4
sxz7728 <sxz7728@sohu.com>
2014-12-09 19:19:55 +0800

    add

--- web-sample-core/src/main/java/sample/core/utils/ColumnUtils.java
+++ web-sample-core/src/main/java/sample/core/utils/ColumnUtils.java
@@ -1,14 +1,18 @@
 package sample.core.utils;
 
+import org.apache.commons.lang.StringUtils;
+
 public class ColumnUtils {
-	public static final String COLUMN = " {0} as {1} ";
+	public static final String COLUMN = " {0} as {1}, ";
 
-	public static final String DICT_VALUE = " (select t1.dictValue from SysDict t1 where t1.dictKey = {0} and t1.dictType = '{1}') as {2} ";
+	public static final String DICT_VALUE = " (select t1.dictValue from SysDict t1 where t1.dictKey = {0} and t1.dictType = '{1}') as {2}, ";
 
-	public static final String USER_NAME = " (select t1.userName from SysUser t1 where t1.id = {0}) as {1} ";
+	public static final String USER_NAME = " (select t1.userName from SysUser t1 where t1.id = {0}) as {1}, ";
 
 	public static String column(String name) {
-		return "";
+		int index = StringUtils.indexOf(name, ".");
+		String alias = index != -1 ? StringUtils.substring(name, index) : name;
+		return Utilities.format(COLUMN, name, alias);
 	}
 
 	public static String column(String name, String alias) {

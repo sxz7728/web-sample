66b5cd76cc816f40b84e58d5f9ab3020ca41805d
sxz7728 <sxz7728@sohu.com>
2014-12-12 15:23:17 +0800

    add

--- web-sample-springmvc/src/main/java/sample/view/controller/test/HelloWorldController.java
+++ web-sample-springmvc/src/main/java/sample/view/controller/test/HelloWorldController.java
@@ -1,10 +1,14 @@
 package sample.view.controller.test;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/test/*")
 public class HelloWorldController {
 
+	@RequestMapping("helloWorld")
+	public void helloWorld(Model model) {
+	}
 }
---
+++ web-sample-springmvc/src/main/webapp/WEB-INF/content/test/helloWorld.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" pageEncoding="UTF-8"%>
+<%@ page contentType="text/html;charset=UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+<script type="text/javascript">
+	
+</script>
+</head>
+<body>hello world
+</body>
+</html>

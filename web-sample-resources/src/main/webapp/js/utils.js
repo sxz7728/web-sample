df6b6ca713588b0cf5b7914b7605b12e1dd39fb0
sxz7728 <sxz7728@sohu.com>
2014-12-18 14:16:40 +0800

    add

--- web-sample-resources/src/main/webapp/js/utils.js
+++ web-sample-resources/src/main/webapp/js/utils.js
@@ -34,26 +34,26 @@
 	$._ajax = function(options) {
 		var opts = jQuery.extend({}, $._ajax.defaults, options);
 
-		$.ajax({
-			type : "post",
-			url : $._url(opts.url),
-			data : opts.params,
-			dataType : "json",
-			success : function(result) {
-				if (result.success) {
-					opts.success(result);
-				} else {
-					if (result.error) {
-						$._notify({
-							message : result.error
-						});
-					}
+		opts.url = $._url(opts.url);
+		opts.data = opts.params;
+		opts.dataType = "json";
+		opts.success = function(result) {
+			if (result.success) {
+				opts.success(result);
+			} else {
+				if (result.error) {
+					$._notify({
+						message : result.error
+					});
 				}
 			}
-		});
+		};
+
+		$.ajax(opts);
 	};
 
 	$._ajax.defaults = {
+		type : "post",
 		params : {},
 		success : function() {
 		}
@@ -69,33 +69,66 @@
 
 	$.fn._ajaxSubmit = function(options) {
 		var opts = jQuery.extend({}, $.fn._ajaxSubmit.defaults, options);
+		var $this = $(this);
 
-		$(this).ajaxSubmit({
-			type : "post",
-			url : $._url(opts.url),
-			data : opts.params,
-			dataType : "json",
-			beforeSubmit : function(arr, $form, options) {
-				return $form.valid();
-			},
-
-			success : function(result) {
-				if (result.success) {
-					opts.success(result);
-				} else {
-					$._notify({
-						message : result.error
-					});
-				}
+		opts.url = $._url(opts.url);
+		opts.data = opts.params;
+		opts.dataType = "json";
+		opts.beforeSubmit = function(arr, $form, options) {
+			return $form.valid();
+		};
+		opts.success = function(result) {
+			if (result.success) {
+				opts.success(result);
+			} else {
+				$._notify({
+					message : result.error
+				});
 			}
-		});
+		};
 
+		$this.ajaxSubmit(opts);
 	};
 
 	$.fn._ajaxSubmit.defaults = {
+		type : "post",
 		params : {},
 		success : function() {
 		}
 	};
 
+	$.fn._jsonSelect = function(options) {
+		var opts = jQuery.extend({}, $.fn._jsonSelect.defaults, options);
+		var $this = $(this);
+
+		$this.find("option[value != '']").remove();
+
+		$.each(opts.data, function(i, val) {
+			$this.append("<option value='" + val.key + "'>" + val.value
+					+ "</option>");
+		});
+
+		$this.val(opts.value);
+
+		if ($this.find("option:selected").size() == 0) {
+			$this.find("option:first").prop('selected', 'selected');
+		}
+	};
+
+	$.fn._jsonSelect.defaults = {
+		data : [],
+		value : ''
+	};
+
+	$.fn._ajaxSelect = function(options) {
+		var opts = jQuery.extend({}, $.fn._ajaxSelect.defaults, options);
+		var $this = $(this);
+
+		opts.success = function(result) {
+			opts.data = result;
+			$this._jsonSelect(opts);
+		};
+	};
+
+	$.fn._ajaxSelect.defaults = {};
 })(jQuery);
